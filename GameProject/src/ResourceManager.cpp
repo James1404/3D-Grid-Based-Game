2179b3a54950fe02862d79d3fc842bef64aa457f
James Barnfather <jimboblony@outlook.com>
2021-06-09 21:25:16 +0100

    Created ResourceManager class. Can load and generate textures from it.

---
+++ GameProject/src/ResourceManager.cpp
@@ -0,0 +1,26 @@
+#include "ResourceManager.h"
+#include <GL/glew.h>
+#include "stb_image.h"
+
+unsigned int ResourceManager::LoadTexture(const char* path) {
+	unsigned int texture;
+
+	// Load and Generate Textures
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	stbi_set_flip_vertically_on_load(true);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+	stbi_image_free(data);
+
+	return texture;
+}
---
+++ GameProject/src/ResourceManager.h
@@ -0,0 +1,5 @@
+#pragma once
+class ResourceManager {
+public:
+	static unsigned int LoadTexture(const char* path);
+};
