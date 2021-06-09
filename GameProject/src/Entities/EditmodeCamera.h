9e2526482fe5a02a9c79010d8d9b41560428d4e1
James Barnfather <jimboblony@outlook.com>
2021-06-09 21:24:09 +0100

    Added new edit mode camera class.

---
+++ GameProject/src/Entities/EditmodeCamera.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "Entity.h"
+#include "../Game.h"
+
+class EditmodeCamera : public Entity {
+public:
+	void update() override {
+		if (Game::event.type == SDL_KEYDOWN) {
+			switch (Game::event.key.keysym.sym) {
+			case SDLK_w:
+				this->velocity.y = speed;
+				break;
+			case SDLK_s:
+				this->velocity.y = -speed;
+				break;
+			case SDLK_a:
+				this->velocity.x = -speed;
+				break;
+			case SDLK_d:
+				this->velocity.x = speed;
+				break;
+			default:
+				break;
+			}
+		}
+
+		if (Game::event.type == SDL_KEYUP) {
+			switch (Game::event.key.keysym.sym) {
+			case SDLK_w:
+				this->velocity.y = 0;
+				break;
+			case SDLK_s:
+				this->velocity.y = 0;
+				break;
+			case SDLK_a:
+				this->velocity.x = 0;
+				break;
+			case SDLK_d:
+				this->velocity.x = 0;
+				break;
+			default:
+				break;
+			}
+		}
+
+		Game::view = glm::translate(Game::view, glm::vec3(velocity, 0.0f));
+	}
+private:
+	glm::vec2 velocity;
+	float speed = 1.0f;
+};
