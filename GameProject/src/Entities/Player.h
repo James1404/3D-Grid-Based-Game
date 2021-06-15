1d2651359c5df9923330dbfc887c2ff8c6ce0612
James Barnfather <jimboblony@outlook.com>
2021-06-15 21:32:06 +0100

    Added Collision detection.

--- GameProject/src/Entities/Player.h
+++ GameProject/src/Entities/Player.h
@@ -1,18 +1,20 @@
 #pragma once
 #include "Entity.h"
+#include "Sprite.h"
+
 #include "../Game.h"
 #include "../SpriteRenderer.h"
+#include "../Collision.h"
 
 class Player : public Entity {
 public:
-	Player() { }
-
 	void init() override {
-		renderer = new SpriteRenderer();
-		renderer->InitSprite();
+		this->renderer.InitSprite();
+
+		CollisionManager::AddCollider("playerCollider", this->position, { 100,100 });
 	}
 
-	void update() override {
+	void update(double dt) {
 		if (Game::event.type == SDL_KEYDOWN) {
 			switch (Game::event.key.keysym.sym) {
 			case SDLK_w:
@@ -31,7 +33,7 @@ public:
 				break;
 			}
 		}
-		
+
 		if (Game::event.type == SDL_KEYUP) {
 			switch (Game::event.key.keysym.sym) {
 			case SDLK_w:
@@ -51,18 +53,29 @@ public:
 			}
 		}
 
-		this->position.x += this->velocity.x * Game::deltaTime;
-		this->position.y += this->velocity.y * Game::deltaTime;
-		glm::normalize(position);
+		CollisionManager::UpdateCollider("playerCollider", this->position + this->velocity, { 100,100 });
+
+		for (auto collider : CollisionManager::colliders) {
+			if (collider.first != "playerCollider") {
+				if (CollisionManager::colliders["playerCollider"].isCollision(collider.second)) {
+					velocity *= -1;
+					printf("isColliding");
+				}
+			}
+		}
+
+		this->position.x += this->velocity.x * dt;
+		this->position.y += this->velocity.y * dt;
 	}
 
 	void render() override {
-		renderer->DrawSprite(position);
+		this->renderer.DrawSprite(position);
 	}
-private:
-	SpriteRenderer* renderer;
 
 	glm::vec2 position;
 	glm::vec2 velocity;
+private:
+	SpriteRenderer renderer;
+
 	float speed = 1.0f;
 };
