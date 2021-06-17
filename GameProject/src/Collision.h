5f282a82c29359c2a56799120c6a5845de7fefc6
James Barnfather <jimboblony@outlook.com>
2021-06-17 21:50:14 +0100

    seperated into .h and .cpp

--- GameProject/src/Collision.h
+++ GameProject/src/Collision.h
@@ -8,38 +8,14 @@ struct Collider {
 	glm::vec2 pos;
 	glm::vec2 size;
 
-	bool isCollision(const Collider& col) {
-		return this->pos.x < col.pos.x + col.size.x &&
-			this->pos.x + this->size.x > col.pos.x &&
-			this->pos.y < col.pos.y + col.size.y &&
-			this->pos.y + this->size.y > col.pos.y;
-	}
-
-	glm::vec2 CalculateDistance(const Collider& col) {
-        glm::vec2 d = { 0,0 };
-
-        if (this->pos.x < col.pos.x) {
-            d.x = col.pos.x - (this->pos.x + this->size.x);
-        }
-        else if (this->pos.x > col.pos.x) {
-            d.x = this->pos.x - (col.pos.x + col.size.x);
-        }
-
-        if (this->pos.y < col.pos.y) {
-            d.y = col.pos.y - (this->pos.y + this->size.y);
-        }
-        else if (this->pos.y > col.pos.y) {
-            d.y = this->pos.y - (col.pos.y + col.size.y);
-        }
-
-        return d;
-	}
+    bool isCollision(const Collider& col);
+    glm::vec2 CalculateDistance(const Collider& col);
 };
 
 class CollisionManager {
 public:
-    static std::map<std::string, Collider> colliders;
+    static std::map<int, Collider> colliders;
 
-    static void AddCollider(std::string name, glm::vec2 pos, glm::vec2 size);
-    static void UpdateCollider(std::string name, glm::vec2 pos, glm::vec2 size);
+    static void AddCollider(int id, glm::vec2 pos, glm::vec2 size);
+    static void UpdateCollider(int id, glm::vec2 pos, glm::vec2 size);
 };
