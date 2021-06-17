415f219a39f94a056e434eda3b06a7831c4cc2ee
James Barnfather <jimboblony@outlook.com>
2021-06-17 21:51:49 +0100

    Added constructor and deconstructor. Added Id to identify the entity.

---
+++ GameProject/src/Entities/Entity.cpp
@@ -0,0 +1,3 @@
+#include "Entity.h"
+
+int Entity::currentID = 0;
--- GameProject/src/Entities/Entity.h
+++ GameProject/src/Entities/Entity.h
@@ -1,7 +1,14 @@
 #pragma once
 class Entity {
 public:
+	int id;
+
 	virtual void init() {}
 	virtual void update(double dt) {}
 	virtual void render() {}
+private:
+	static int currentID;
+protected:
+	Entity() : id(currentID++) { }
+	~Entity() { }
 };
