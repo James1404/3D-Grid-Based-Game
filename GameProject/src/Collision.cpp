7ed1ada33f21f2d1da3f99457b9018550af6a089
James Barnfather <jimboblony@outlook.com>
2021-06-17 21:50:34 +0100

    Now accepts entity id instead of name.

--- GameProject/src/Collision.cpp
+++ GameProject/src/Collision.cpp
@@ -1,16 +1,45 @@
 #include "Collision.h"
 
-std::map<std::string, Collider> CollisionManager::colliders;
+std::map<int, Collider> CollisionManager::colliders;
 
-void CollisionManager::AddCollider(std::string name, glm::vec2 pos, glm::vec2 size) {
+void CollisionManager::AddCollider(int id, glm::vec2 pos, glm::vec2 size) {
     Collider col;
     col.pos = pos;
     col.size = size;
 
-    colliders[name] = col;
+    colliders[id] = col;
 }
 
-void CollisionManager::UpdateCollider(std::string name, glm::vec2 pos, glm::vec2 size) {
-    colliders[name].pos = pos;
-    colliders[name].size = size;
+void CollisionManager::UpdateCollider(int id, glm::vec2 pos, glm::vec2 size) {
+    colliders[id].pos = pos;
+    colliders[id].size = size;
+}
+
+bool Collider::isCollision(const Collider& col)
+{
+    return this->pos.x < col.pos.x + col.size.x &&
+        this->pos.x + this->size.x > col.pos.x &&
+        this->pos.y < col.pos.y + col.size.y &&
+        this->pos.y + this->size.y > col.pos.y;
+}
+
+glm::vec2 Collider::CalculateDistance(const Collider& col)
+{
+    glm::vec2 d = { 0,0 };
+
+    if (this->pos.x < col.pos.x) {
+        d.x = col.pos.x - (this->pos.x + this->size.x);
+    }
+    else if (this->pos.x > col.pos.x) {
+        d.x = this->pos.x - (col.pos.x + col.size.x);
+    }
+
+    if (this->pos.y < col.pos.y) {
+        d.y = col.pos.y - (this->pos.y + this->size.y);
+    }
+    else if (this->pos.y > col.pos.y) {
+        d.y = this->pos.y - (col.pos.y + col.size.y);
+    }
+
+    return d;
 }
