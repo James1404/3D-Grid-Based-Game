d55aa3aa4d5b1b5424c0be87cd2a464e01e5b43d
James Barnfather <jimboblony@outlook.com>
2021-06-23 17:45:22 +0100

    Added load and save functions.

--- GameProject/src/Entities/Entity.h
+++ GameProject/src/Entities/Entity.h
@@ -1,14 +1,20 @@
 #pragma once
+#include <stdio.h>
+#include <fstream>
+
 class Entity {
 public:
 	int id;
+
+	Entity() : id(currentID++) { printf("Entity %i Created\n", id); }
+	~Entity() { printf("Entity %i Destroyed\n", id); }
+
+	virtual void save(std::ofstream& f) {}
+	virtual void load(std::ifstream& f) {}
 
 	virtual void init() {}
 	virtual void update(double dt) {}
 	virtual void render() {}
 private:
 	static int currentID;
-protected:
-	Entity() : id(currentID++) { }
-	~Entity() { }
 };
