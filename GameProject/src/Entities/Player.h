7d21c6b19d446ccf99159dc0f234e6e8139790f7
James Barnfather <jimboblony@outlook.com>
2021-06-25 16:42:05 +0100

    Fixed toolbars affecting all entites.

--- GameProject/src/Entities/Player.h
+++ GameProject/src/Entities/Player.h
@@ -87,8 +87,8 @@ public:
 
 	void editmodeRender() override {
 		ImGui::Text("Player");
-		ImGui::DragFloat("Position.x", &this->position.x);
-		ImGui::DragFloat("Position.y", &this->position.y);
+		ImGui::DragFloat("Position.x##" + this->id, &this->position.x);
+		ImGui::DragFloat("Position.y##" + this->id, &this->position.y);
 	}
 
 	glm::vec2 position;
--- GameProject/src/Entities/Sprite.h
+++ GameProject/src/Entities/Sprite.h
@@ -35,8 +35,8 @@ public:
 
 	void editmodeRender() override {
 		ImGui::Text("Sprite");
-		ImGui::DragFloat("Position.x", &this->position.x);
-		ImGui::DragFloat("Position.y", &this->position.y);
+		ImGui::DragFloat("Position.x##" + this->id, &this->position.x);
+		ImGui::DragFloat("Position.y##" + this->id, &this->position.y);
 	}
 private:
 	SpriteRenderer renderer;
