d30750da58fd1443321aa4ff5bcb1bfebc52b14f
James Barnfather <jimboblony@outlook.com>
2021-07-10 13:58:05 +0100

    Updated Editor.

--- GameProject/src/Entities/Editor.h
+++ GameProject/src/Entities/Editor.h
@@ -63,42 +63,25 @@ public:
 		ImGui_ImplSDL2_NewFrame(Game::window);
 		ImGui::NewFrame();
 
+		const float PAD = 10.0f;
+		static bool p_open = true;
+
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(PAD, PAD));
+
 		{
 			const ImGuiViewport* viewport = ImGui::GetMainViewport();
-			ImVec2 work_pos = viewport->WorkPos;
 			ImVec2 work_size = viewport->WorkSize;
-			ImVec2 window_pos, window_pos_pivot;
 
-			const float PAD = 10.0f;
-
-			window_pos.x = work_pos.x + PAD;
-			window_pos.y = work_pos.y + PAD;
-			window_pos_pivot.x = 0.0f;
-			window_pos_pivot.y = 0.0f;
-
-			ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+			ImGui::SetNextWindowPos(ImVec2(PAD, PAD), ImGuiCond_Always, ImVec2(0.0f, 0.0f));
 
 			ImGuiWindowFlags window_flags = 0;
 			window_flags |= ImGuiWindowFlags_NoDecoration;
 			window_flags |= ImGuiWindowFlags_AlwaysAutoResize;
 			window_flags |= ImGuiWindowFlags_NoFocusOnAppearing;
 			window_flags |= ImGuiWindowFlags_NoNav;
-			window_flags |= ImGuiWindowFlags_MenuBar;
 
-			static bool p_open = true;
 			ImGui::SetNextWindowBgAlpha(0.9f);
 			if (ImGui::Begin("Overlay", &p_open, window_flags)) {
-				if (ImGui::BeginMenuBar()) {
-					if (ImGui::BeginMenu("File")) {
-						if (ImGui::MenuItem("New")) { Game::scene.newScene(); }
-						if (ImGui::MenuItem("Save")) { Game::scene.saveScene(); }
-						if (ImGui::MenuItem("Load")) { Game::scene.loadScene(); }
-						ImGui::EndMenu();
-					}
-
-					ImGui::EndMenuBar();
-				}
-
 				ImGui::Text("Game Stats");
 
 				ImGui::Separator();
@@ -111,11 +94,7 @@ public:
 
 		{
 			const ImGuiViewport* viewport = ImGui::GetMainViewport();
-
-			ImVec2 work_pos = viewport->WorkPos;
 			ImVec2 work_size = viewport->WorkSize;
-
-			const float PAD = 10.0f;
 
 			// Set Windows Flags
 			ImGuiWindowFlags window_flags = 0;
@@ -124,24 +103,33 @@ public:
 			window_flags |= ImGuiWindowFlags_NoCollapse;
 			window_flags |= ImGuiWindowFlags_NoNav;
 			window_flags |= ImGuiWindowFlags_NoFocusOnAppearing;
-
-			static bool p_open = NULL;
+			window_flags |= ImGuiWindowFlags_MenuBar;
 
 			{
 				// Entites List Window
-				ImGui::SetNextWindowPos(ImVec2(work_pos.x + work_size.x - PAD, work_pos.y + PAD), ImGuiCond_Always, ImVec2(1.0f, 0.0f));
-				ImGui::SetNextWindowSize(ImVec2(300.0f, (work_size.y / 2) - work_pos.y - (PAD * 1.5f)), ImGuiCond_Always);
+				ImGui::SetNextWindowPos(ImVec2(work_size.x - PAD, PAD), ImGuiCond_Always, ImVec2(1.0f, 0.0f));
+				ImGui::SetNextWindowSize(ImVec2(300.0f, (work_size.y / 2) - (PAD * 1.5f)), ImGuiCond_Always);
 
 				ImGui::SetNextWindowBgAlpha(0.9f);
 
 				if (ImGui::Begin("Entities", &p_open, window_flags)) {
-					if (ImGui::BeginPopupContextWindow()) {
-						if (ImGui::MenuItem("Create Sprite")) { Game::scene.CreateSprite(); selectedEntity = Game::scene.entities.back(); }
-						if (ImGui::MenuItem("Create Player")) { Game::scene.CreatePlayer(); selectedEntity = Game::scene.entities.back(); }
-						ImGui::EndPopup();
+					if (ImGui::BeginMenuBar()) {
+						if (ImGui::BeginMenu("File")) {
+							if (ImGui::MenuItem("New")) { Game::scene.newScene(); }
+							if (ImGui::MenuItem("Save")) { Game::scene.saveScene(); }
+							if (ImGui::MenuItem("Load")) { Game::scene.loadScene(); }
+							ImGui::EndMenu();
+						}
+
+						if (ImGui::BeginMenu("Create")) {
+							if (ImGui::MenuItem("Create Sprite")) { Game::scene.CreateSprite(); selectedEntity = Game::scene.entities.back(); }
+							if (ImGui::MenuItem("Create Player")) { Game::scene.CreatePlayer(); selectedEntity = Game::scene.entities.back(); }
+							ImGui::EndMenu();
+						}
+						ImGui::EndMenuBar();
 					}
 
-					if (ImGui::ListBoxHeader("Entities", ImVec2(300.0f, (work_size.y / 4) + PAD))) {
+					if (ImGui::ListBoxHeader("Entities", ImVec2(300.0f, ((work_size.y / 2) - (PAD * 6))))) {
 						for (auto const& entity : Game::scene.entities) {
 							const bool is_selected = (selectedEntity != nullptr) && (selectedEntity->id == entity->id);
 
@@ -162,23 +150,36 @@ public:
 					ImGui::End();
 				}
 			}
+		}
 
-			{
-				// Toolbar Window
-				ImGui::SetNextWindowPos(ImVec2(work_pos.x + work_size.x - PAD, work_size.y - PAD), ImGuiCond_Always, ImVec2(1.0f, 1.0f));
-				ImGui::SetNextWindowSize(ImVec2(300.0f, (work_size.y / 2) + work_pos.y - (PAD * 1.5f)), ImGuiCond_Always);
+		{
+			const ImGuiViewport* viewport = ImGui::GetMainViewport();
+			ImVec2 work_size = viewport->WorkSize;
 
-				ImGui::SetNextWindowBgAlpha(0.9f);
+			// Set Windows Flags
+			ImGuiWindowFlags window_flags = 0;
+			window_flags |= ImGuiWindowFlags_NoDecoration;
+			window_flags |= ImGuiWindowFlags_NoResize;
+			window_flags |= ImGuiWindowFlags_NoCollapse;
+			window_flags |= ImGuiWindowFlags_NoNav;
+			window_flags |= ImGuiWindowFlags_NoFocusOnAppearing;
 
-				if (ImGui::Begin("Toolbar", &p_open, window_flags)) {
-					if (selectedEntity != nullptr) {
-						selectedEntity->editmodeRender();
-					}
+			// Toolbar Window
+			ImGui::SetNextWindowPos(ImVec2(work_size.x - PAD, work_size.y - PAD), ImGuiCond_Always, ImVec2(1.0f, 1.0f));
+			ImGui::SetNextWindowSize(ImVec2(300.0f, (work_size.y / 2) - (PAD * 1.5f)), ImGuiCond_Always);
 
-					ImGui::End();
+			ImGui::SetNextWindowBgAlpha(0.9f);
+
+			if (ImGui::Begin("Toolbar", &p_open, window_flags)) {
+				if (selectedEntity != nullptr) {
+					selectedEntity->editmodeRender();
 				}
+
+				ImGui::End();
 			}
 		}
+		ImGui::PopStyleVar();
+
 		ImGui::EndFrame();
 
 		ImGui::Render();
