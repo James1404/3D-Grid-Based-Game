0d265b5feb5ee767166089b6f538a5fdfab24f0c
James1404 <jimboblony@outlook.com>
2021-08-15 14:12:52 +0100

    Added Layers enum.

--- GameProject/src/Entity.h
+++ GameProject/src/Entity.h
@@ -4,10 +4,26 @@
 
 #include <json.hpp>
 
+enum class Layers {
+	Background = -10,
+	Foreground = 10,
+
+	StaticCollider = 2,
+
+	Player = 0,
+	Enemies = 0,
+	
+	Interactable = -2
+};
+
+// TODO: Implement UUID System.
+
 class Entity {
 public:
 	int id;
 	char name[128] = "name";
+
+	Layers layer;
 
 	Entity() : id(currentID++) { printf("Entity %i Created\n", id); }
 	~Entity() { printf("Entity %i Destroyed\n", id); }
