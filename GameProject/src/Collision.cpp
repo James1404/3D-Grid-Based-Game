71cd8512848464e7ec5a878082efedb2f7d32841
James Barnfather <jimboblony@outlook.com>
2021-08-27 14:02:37 +0100

    Added references to owners.

--- GameProject/src/Collision.cpp
+++ GameProject/src/Collision.cpp
@@ -1,18 +1,21 @@
 #include "Collision.h"
 
 std::vector<Collider*> CollisionManager::colliders;
-unsigned int Collider::currentID = 0;
 
 Collider::Collider()
-    : id(currentID++), pos(0,0), size(0,0) {
+    : pos(0,0), size(0,0) {
     CollisionManager::colliders.push_back(this);
 }
 
 Collider::~Collider() { }
 
+void Collider::InitCollider(Entity* _owner) {
+    this->owner = _owner;
+}
+
 bool Collider::ColliderVsCollider() {
     for (const auto& collider : CollisionManager::colliders) {
-        if (collider->id != this->id) {
+        if (collider->owner->id != this->owner->id) {
             if (this->pos.x < collider->pos.x + collider->size.x &&
                 this->pos.x + this->size.x >    collider->pos.x &&
                 this->pos.y < collider->pos.y + collider->size.y &&
@@ -25,46 +28,58 @@ bool Collider::ColliderVsCollider() {
     return false;
 }
 
+Ray::Ray(Entity* _owner, glm::vec2 _origin, glm::vec2 _direction) {
+    this->owner = _owner;
+    this->origin = _origin;
+    this->direction = _direction;
+}
+
+Ray::~Ray() { }
+
 // TODO: Fix Raycasting Implementation
 bool Ray::RayVsCollider(RayHit& hit) {
     for (const auto& collider : CollisionManager::colliders) {
-        hit.normal = { 0,0 };
-        hit.point = { 0,0 };
+        if (collider->owner->id != this->owner->id) {
+            hit.normal = { 0,0 };
+            hit.point = { 0,0 };
 
-        glm::vec2 invdir = 1.0f / this->direction;
+            glm::vec2 invdir = 1.0f / this->direction;
 
-        glm::vec2 t_near = (collider->pos - this->origin) * invdir;
-        glm::vec2 t_far = (collider->pos + collider->size - this->origin) * invdir;
+            glm::vec2 t_near = (collider->pos - this->origin) * invdir;
+            glm::vec2 t_far = (collider->pos + collider->size - this->origin) * invdir;
 
-        if (std::isnan(t_far.y) || std::isnan(t_far.x)) continue;
-        if (std::isnan(t_near.y) || std::isnan(t_near.x)) continue;
+            if (std::isnan(t_far.y) || std::isnan(t_far.x)) continue;
+            if (std::isnan(t_near.y) || std::isnan(t_near.x)) continue;
 
-        if (t_near.x > t_far.x) std::swap(t_near.x, t_far.x);
-        if (t_near.y > t_far.y) std::swap(t_near.y, t_far.y);
+            if (t_near.x > t_far.x) std::swap(t_near.x, t_far.x);
+            if (t_near.y > t_far.y) std::swap(t_near.y, t_far.y);
 
-        if (t_near.x > t_far.y || t_near.y > t_far.x) continue;
+            if (t_near.x > t_far.y || t_near.y > t_far.x) continue;
 
-        hit.distance = std::max(t_near.x, t_near.y);
+            hit.distance = std::max(t_near.x, t_near.y);
 
-        float t_hit_far = std::min(t_far.x, t_far.y);
+            float t_hit_far = std::min(t_far.x, t_far.y);
 
-        if (t_hit_far < 0)
-            continue;
+            if (t_hit_far < 0)
+                continue;
 
-        hit.point = this->origin + hit.distance * this->direction;
+            hit.point = this->origin + hit.distance * this->direction;
 
-        if (t_near.x > t_near.y)
-            if (invdir.x < 0)
-                hit.normal = { 1, 0 };
-            else
-                hit.normal = { -1, 0 };
-        else if (t_near.x < t_near.y)
-            if (invdir.y < 0)
-                hit.normal = { 0, 1 };
-            else
-                hit.normal = { 0, -1 };
+            if (t_near.x > t_near.y)
+                if (invdir.x < 0)
+                    hit.normal = { 1, 0 };
+                else
+                    hit.normal = { -1, 0 };
+            else if (t_near.x < t_near.y)
+                if (invdir.y < 0)
+                    hit.normal = { 0, 1 };
+                else
+                    hit.normal = { 0, -1 };
 
-        return true;
+            hit.collider = collider;
+
+            return true;
+        }
     }
 
     return false;
--- GameProject/src/Collision.h
+++ GameProject/src/Collision.h
@@ -4,29 +4,37 @@
 #include <map>
 #include <string>
 
+#include "Entity.h"
+
 // TODO: Have Collide Use Owners ID
 struct Collider {
     Collider();
     ~Collider();
 
-    unsigned int id;
+    void InitCollider(Entity* _owner);
+
+    Entity* owner;
 
     glm::vec2 pos;
     glm::vec2 size;
 
     bool ColliderVsCollider();
-private:
-    static unsigned int currentID;
 };
 
 struct RayHit {
     glm::vec2 point;
     glm::vec2 normal;
     float distance;
+    Collider* collider;
 };
 
 // TODO: Have Ray Use Owners ID
 struct Ray {
+    Ray(Entity* _owner, glm::vec2 _origin, glm::vec2 _direction);
+    ~Ray();
+
+    Entity* owner;
+
     glm::vec2 origin;
     glm::vec2 direction;
 
