b8f1bfb1ee79abfc22cdc3870d16b88aec625d6f
James Barnfather <jimboblony@outlook.com>
2021-09-26 17:29:27 +0100

    added path and lerping

--- GameProject/src/player.cpp
+++ GameProject/src/player.cpp
@@ -6,8 +6,18 @@
 #include <json.hpp>
 
 player::player_data player::data;
+player::path player::player_path;
+
+float lerpTime = 1.0f;
+float currentLerpTime;
+
+glm::vec2 lerp(const glm::vec2& A, const glm::vec2& B, float t) {
+	// return A * t + B * (1.f - t);
+	return A + (B - A) * t;
+}
 
 void player::init() {
+	printf("------------------\n");
 	data.spr = renderer::create_sprite("data/textures/player.png", &data.pos, 1);
 	data.col = collision::create_collider({ data.spr->width, data.spr->height });
 
@@ -34,6 +44,20 @@ void player::update(double dt) {
 	else if (input::button_pressed("MoveRight")) { data.vel.x = 1; }
 	else { data.vel.x = 0; }
 
+	currentLerpTime += dt;
+	if (currentLerpTime > lerpTime)
+		currentLerpTime = lerpTime;
+
+	float perc = currentLerpTime / lerpTime;
+
+	if (perc > 1) {
+		data.current_node += 1;
+		currentLerpTime = 0;
+	}
+
+	data.pos = lerp(data.pos, player_path.nodes[data.current_node + 1], perc);
+
+	/*
 	float movementSpeed = data.speed;
 	if (input::button_pressed("Run")) { movementSpeed *= .5f; }
 
@@ -47,11 +71,12 @@ void player::update(double dt) {
 	}
 
 	data.pos += moveVector;
+	*/
 }
 
 void player::clean() {
+	printf("--------------\n");
 	renderer::delete_sprite(data.spr);
 	collision::delete_collider(data.col);
-
 	printf("PLAYER CLEANED\n");
 }
--- GameProject/src/player.h
+++ GameProject/src/player.h
@@ -3,91 +3,21 @@
 #include "renderer.h"
 #include "collision.h"
 
-/*
-struct player : public entity {
-	glm::vec2 pos;
-	glm::vec2 vel;
-	renderer::sprite* spr;
-	collider* col;
-	float speed = 50;
-	player() {
-		strcpy_s(name, "Player");
-
-		spr = renderer::create_sprite("data/textures/player.png", &pos, 1);
-		col = create_collider(this, { spr->width, spr->height });
-	}
-
-	~player() {
-		renderer::delete_sprite(spr);
-		delete_collider(col);
-	}
-
-	void update(double dt) {
-		pos.y = 0;
-
-		if (input::button_pressed("Aim")) {
-			if (input::button_down("Shoot")) {
-				printf("Shoot\n");
-
-				ray_data hit;
-				if (ray_vs_collider(this, hit, pos, { 20,0 })) {
-					printf("You Hit %s\n", hit.col->owner->name);
-				}
-			}
-
-			return;
-		}
-
-		if (input::button_pressed("MoveLeft")) { vel.x = -1; }
-		else if (input::button_pressed("MoveRight")) { vel.x = 1; }
-		else { vel.x = 0; }
-
-		float movementSpeed = speed;
-		if (input::button_pressed("Run")) { movementSpeed *= .5f; }
-
-		glm::vec2 moveVector = glm::vec2(std::floor(vel.x), std::floor(vel.y));
-		moveVector /= movementSpeed;
-		moveVector *= dt;
-
-		col->pos = pos + moveVector;
-		if (collider_vs_collider(col)) {
-			return;
-		}
-
-		this->pos += moveVector;
-	}
-
-	void serialize_entity(nlohmann::json& j) override {
-		j["Player"] += {
-			{"name", name},
-			{ "position.x", pos.x },
-			{ "position.y", pos.y }
-		};
-	}
-
-#ifdef _DEBUG
-	void editor_draw() override {
-		ImGui::PushID(id);
-		ImGui::InputText("Name", name, IM_ARRAYSIZE(name));
-		ImGui::Text("ID: %i", id);
-
-		ImGui::Separator();
-
-		ImGui::DragFloat("Position.x", &pos.x);
-		ImGui::DragFloat("Position.y", &pos.y);
-		ImGui::PopID();
-	}
-#endif // _DEBUG
-};
-*/
-
 namespace player {
+	struct path {
+		std::vector<glm::vec2> nodes = { {0,0}, {200, 50}, {80, 100} };
+	};
+
+	extern path player_path;
+
 	struct player_data {
 		glm::vec2 pos = { 0,0 };
 		glm::vec2 vel = { 0,0 };
 		renderer::sprite* spr = nullptr;
 		collision::box_collider* col = nullptr;
 		float speed = 50;
+
+		unsigned int current_node = 0;
 	};
 
 	extern player_data data;
