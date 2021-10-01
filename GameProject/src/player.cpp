1f99c816fa990be3d959ba2978c7f94c83b7c9f8
James Barnfather <jimboblony@outlook.com>
2021-10-01 20:27:00 +0100

    Replaced distance function with glm function. Removed unneccasary includes and code.

--- GameProject/src/player.cpp
+++ GameProject/src/player.cpp
@@ -1,20 +1,9 @@
 #include "player.h"
 
 #include "input.h"
-
-#include <imgui.h>
-#include <json.hpp>
-
-#include <cmath>
+#include "scene.h"
 
 player::player_data player::data;
-player::path player::player_path;
-
-float distance(const glm::vec2 a, const glm::vec2 b) {
-	float x_diff = a.x - b.x;
-	float y_diff = a.y - b.y;
-	return std::sqrt(x_diff * x_diff + y_diff * y_diff);
-}
 
 void player::init() {
 	printf("------------------\n");
@@ -46,45 +35,47 @@ void player::update(double dt) {
 	float movementSpeed = .1f;
 	if (input::button_pressed("Run")) { movementSpeed = .5f; }
 
-	if (data.vel.x > 0) {
-		if (current_node != player_path.nodes.size() - 1) {
-			if (distance(data.pos, player_path.nodes[current_node + 1]) > 0.1) {
-				glm::vec2 dir = glm::normalize(player_path.nodes[current_node + 1] - data.pos);
-				glm::vec2 move_vector = dir * data.vel.x;
+	if (!level::data.path_nodes.empty()) {
+		if (data.vel.x > 0) {
+			if (current_node != level::data.path_nodes.size() - 1) {
+				if (glm::distance(data.pos, level::data.path_nodes[current_node + 1]) > 0.01) {
+					glm::vec2 dir = glm::normalize(level::data.path_nodes[current_node + 1] - data.pos);
+					glm::vec2 move_vector = dir * data.vel.x;
 
-				move_vector *= dt;
-				move_vector *= movementSpeed;
+					move_vector *= dt;
+					move_vector *= movementSpeed;
 
-				data.col->pos = data.pos + move_vector;
-				if (collider_vs_collider(data.col)) {
-					return;
+					data.col->pos = data.pos + move_vector;
+					if (collider_vs_collider(data.col)) {
+						return;
+					}
+
+					data.pos += move_vector;
 				}
-
-				data.pos += move_vector;
-			}
-			else {
-				current_node++;
+				else {
+					current_node++;
+				}
 			}
 		}
-	}
-	else {
-		if (current_node >= 0) {
-			if (distance(data.pos, player_path.nodes[current_node]) > 0.01) {
-				glm::vec2 dir = glm::normalize(player_path.nodes[current_node] - data.pos);
-				glm::vec2 move_vector = dir * data.vel.x;
+		else {
+			if (current_node >= 0) {
+				if (glm::distance(data.pos, level::data.path_nodes[current_node]) > 0.01) {
+					glm::vec2 dir = glm::normalize(level::data.path_nodes[current_node] - data.pos);
+					glm::vec2 move_vector = dir * data.vel.x;
 
-				move_vector *= dt;
-				move_vector *= movementSpeed;
+					move_vector *= dt;
+					move_vector *= movementSpeed;
 
-				data.col->pos = data.pos - move_vector;
-				if (collider_vs_collider(data.col)) {
-					return;
+					data.col->pos = data.pos - move_vector;
+					if (collider_vs_collider(data.col)) {
+						return;
+					}
+
+					data.pos -= move_vector;
 				}
-
-				data.pos -= move_vector;
-			}
-			else {
-				current_node--;
+				else {
+					current_node--;
+				}
 			}
 		}
 	}
