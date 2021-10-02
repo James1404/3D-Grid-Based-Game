8b9167e5ff61bb1b78bd655103edded4b4682403
James Barnfather <jimboblony@outlook.com>
2021-10-02 19:08:51 +0100

    Finished level parsing and serialization.

--- GameProject/src/Scene.cpp
+++ GameProject/src/Scene.cpp
@@ -25,7 +25,7 @@ void level::clean() {
 
 	data.sprites.clear();
 
-	printf("LEVEL CLEANED UP\n");
+	printf("CLEANED LEVEL DATA\n");
 }
 
 unsigned int fileVersion = 1;
@@ -34,14 +34,11 @@ void level::save(std::string level_name) {
 
 	std::ofstream ofs(levelPath, std::ofstream::trunc);
 	if (ofs.is_open()) {
-		ofs << "FILE_VERSION : " << fileVersion << std::endl << std::endl;
-		ofs << "PLAYER : " << "Position (" << (int)player::data.pos.x << ", " << (int)player::data.pos.y << ")" << ";" << std::endl;
-
-		ofs << std::endl;
+		ofs << "FILE_VERSION " << fileVersion << std::endl << std::endl;
 
 		if (!data.obstacles.empty()) {
 			for (auto& _obstacle : data.obstacles) {
-				ofs << "OBSTACLE : " << "Position (" << (int)_obstacle->pos.x << ", " << (int)_obstacle->pos.y << ")" << ";" << std::endl;
+				ofs << "OBSTACLE " << (int)_obstacle->pos.x << " " << (int)_obstacle->pos.y << std::endl;
 			}
 
 			ofs << std::endl;
@@ -49,7 +46,7 @@ void level::save(std::string level_name) {
 
 		if (!data.path_nodes.empty()) {
 			for (auto& _node : data.path_nodes) {
-				ofs << "PATH_NODE : " << "Position (" << (int)_node.x << "," << (int)_node.y << ")" << ";" << std::endl;
+				ofs << "PATH_NODE " << (int)_node.x << " " << (int)_node.y << std::endl;
 			}
 
 			ofs << std::endl;
@@ -57,7 +54,7 @@ void level::save(std::string level_name) {
 
 		if (!data.sprites.empty()) {
 			for (auto& _sprite : data.sprites) {
-				ofs << "SPRITE : " << "Position (" << (int)_sprite->position->x << ", " << (int)_sprite->position->y << ")" << ";" << std::endl;
+				ofs << "SPRITE " << (int)_sprite->position->x << " " << (int)_sprite->position->y << std::endl;
 			}
 
 			ofs << std::endl;
@@ -68,9 +65,10 @@ void level::save(std::string level_name) {
 }
 
 void level::load(std::string level_name) {
-	clean();
 	printf("---------------------\n");
 	printf("RETRIEVING SCENE FILE\n");
+
+	clean();
 
 	std::string levelPath = "data/scenes/" + level_name;
 
@@ -84,9 +82,7 @@ void level::load(std::string level_name) {
 			std::string type;
 			
 			// TODO: Remove white spaces.
-			ss >> std::ws;
-			printf("%s\n", ss.str().c_str());
-			std::getline(ss, type, ':');
+			std::getline(ss, type, ' ');
 
 			if (type == "FILE_VERSION") {
 				int file_version;
@@ -96,49 +92,30 @@ void level::load(std::string level_name) {
 					printf("OUTDATED SAVE FILE\n");
 					return;
 				}
-
-				printf("Scene version is %u\n", file_version);
 			}
-			
-			if (type == "PLAYER") {
+
+			if (type == "OBSTACLE") {
 				glm::ivec2 position;
 				ss >> position.x >> position.y;
 
-				player::data.pos = position;
-				printf("Player Data Loaded\n");
-			} else if (type == "OBSTACLE") {
+				auto o = std::make_shared<obstacle>();
+				o->pos = position;
 
+				data.obstacles.push_back(o);
+			}
+			else if (type == "PATH_NODE") {
+				glm::vec2 node;
+				ss >> node.x >> node.y;
+				data.path_nodes.push_back(node);
 			}
 		}
 
 		printf("FINISHED LOADING SCENE DATA\n");
+		printf("---------------------------\n");
 	}
 	else {
 		printf("CANNOT FIND SCENE FILE\n");
 	}
 
 	ifs.close();
-
-	/*
-	nlohmann::json j;
-	if (!j["Player"].empty()) {
-		for (const auto& data : j["Player"]) {
-			player::data.pos.x = data["position.x"];
-			player::data.pos.y = data["position.y"];
-
-			printf(" - LOADED PLAYER DATA\n");
-		}
-	}
-
-	if (!j["Obstacle"].empty()) {
-		for (const auto& obstacle_j : j["Obstacle"]) {
-			auto o = std::make_shared<obstacle>();
-
-			o->pos.x = obstacle_j["position.x"];
-			o->pos.y = obstacle_j["position.y"];
-
-			data.obstacles.push_back(o);
-		}
-	}
-	*/
 }
