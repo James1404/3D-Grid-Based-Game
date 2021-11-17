c7bdfccd686be8c32df3ba632199c4b35016aabb
James Barnfather <jimboblony@outlook.com>
2021-11-17 00:02:39 +0000

    Removed path_nodes.

--- GameProject/src/scene.cpp
+++ GameProject/src/scene.cpp
@@ -71,14 +71,12 @@ void level::init() {
 }
 
 void level::update(double dt) {
-	for (auto& _node : path_nodes) {
-		for (auto& _obstacle : _node->obstacles) {
-			_obstacle->update(dt);
-		}
+	for (auto& _obstacle : obstacles) {
+		_obstacle->update(dt);
+	}
 
-		for (auto& _enemy : _node->enemies) {
-			_enemy->update(dt);
-		}
+	for (auto& _enemy : enemies) {
+		_enemy->update(dt);
 	}
 	
 	for (auto& _sprite : sprites) {
@@ -91,19 +89,19 @@ void level::update(double dt) {
 }
 
 void level::clean() {
-	path_nodes.clear();
-
 	game_event_manager.clear();
-	cutscenes.clear();
 
+	obstacles.clear();
+	enemies.clear();
 	sprites.clear();
+	cutscenes.clear();
 
 	name.clear();
 
 	printf("CLEANED LEVEL DATA\n");
 }
 
-uint32_t fileVersion = 6;
+uint32_t fileVersion = 8;
 void level::save() {
 	std::string levelPath = "data/scenes/";
 	levelPath.append(name);
@@ -113,48 +111,25 @@ void level::save() {
 	if (ofs.is_open()) {
 		ofs << "FILE_VERSION " << fileVersion << std::endl << std::endl;
 
-		if (!path_nodes.empty()) {
-			for (auto& _node : path_nodes) {
-				if(!_node->obstacles.empty()) {
-					for(auto& _obstacle : _node->obstacles) {
-						ofs << "OBSTACLE" << " " << (int)_obstacle->pos.x << " " << (int)_obstacle->pos.y << std::endl;
-					}
-
-					ofs << std::endl;
-				}
-
-				if(!_node->enemies.empty()){
-					for(auto& _enemy : _node->enemies) {
-						ofs << "ENEMY" << " " << (int)_enemy->pos.x << " " << (int)_enemy->pos.y << std::endl;
-					}
-
-					ofs << std::endl;
-				}
-
-				std::string temp_event_name = "NULL";
-				if(!_node->trigger_event_name.empty())
-					temp_event_name = _node->trigger_event_name;
-				
-				ofs << "PATH_NODE" << " " << (int)_node->pos.x << " " << (int)_node->pos.y << " " << _node->flags << " " << temp_event_name << std::endl;
+		if (!obstacles.empty()) {
+			for (auto& _obstacle : obstacles) {
+				ofs << "OBSTACLE" << " " << (int)_obstacle->pos.x << " " << (int)_obstacle->pos.y << std::endl;
 			}
 
 			ofs << std::endl;
 		}
 
-		/*
 		if (!enemies.empty()) {
 			for (auto& _enemy : enemies) {
-				ofs << "ENEMY" << " " << (int)_enemy->pos.x << " " << (int)_enemy->pos.y << " " << _enemy->current_node << std::endl;
+				ofs << "ENEMY" << " " << (int)_enemy->pos.x << " " << (int)_enemy->pos.y << std::endl;
 			}
 
 			ofs << std::endl;
 		}
-		*/
 
 		if (!sprites.empty()) {
 			for (auto& _sprite : sprites) {
 				ofs << "SPRITE" << " " << (int)_sprite->pos.x << " " << (int)_sprite->pos.y << " " <<
-					(int)_sprite->spr->size.x << " " << (int)_sprite->spr->size.y << " " <<
 					(int)_sprite->spr->layer << " " <<
 					_sprite->spr->colour.x << " " << _sprite->spr->colour.y << " " << _sprite->spr->colour.z << std::endl;
 			}
@@ -188,9 +163,6 @@ void level::load(std::string level_name) {
 	if (ifs.is_open()) {
 		printf("PARSING SCENE FILE\n");
 
-		std::vector<std::shared_ptr<enemy_entity>> temp_enemies;
-		std::vector<std::shared_ptr<obstacle_entity>> temp_obstacles;
-
 		std::string line;
 		while (std::getline(ifs, line)) {
 			std::stringstream ss(line);
@@ -215,7 +187,7 @@ void level::load(std::string level_name) {
 				auto o = std::make_shared<obstacle_entity>();
 				o->pos = position;
 
-				temp_obstacles.push_back(o);
+				obstacles.push_back(o);
 			}
 			else if (type == "ENEMY") {
 				glm::ivec2 position;
@@ -225,43 +197,18 @@ void level::load(std::string level_name) {
 				auto e = std::make_shared<enemy_entity>();
 				e->pos = position;
 
-				temp_enemies.push_back(e);
-			}
-			else if (type == "PATH_NODE") {
-				glm::vec2 pos;
-				PATH_NODE_FLAGS flags;
-				std::string trigger_event_name;
-				ss >> pos.x >> pos.y >> flags >> trigger_event_name;
-
-				auto n = std::make_shared<path_node>();
-				n->pos = pos;
-				n->flags = flags;
-				if (trigger_event_name != "NULL")
-					n->trigger_event_name = trigger_event_name;
-
-				n->enemies = temp_enemies;
-				temp_enemies.clear();
-
-				n->obstacles = temp_obstacles;
-				temp_obstacles.clear();
-
-				path_nodes.push_back(n);
+				enemies.push_back(e);
 			}
 			else if (type == "SPRITE") {
 				glm::ivec2 position;
-				glm::ivec2 size;
 				int layer;
 				glm::vec3 colour;
 				//std::string path;
 
-				ss >> position.x >> position.y
-					>> size.x >> size.y >>
-					layer >>
-					colour.x >> colour.y >> colour.z;
+				ss >> position.x >> position.y >> layer >> colour.x >> colour.y >> colour.z;
 
 				auto s = std::make_shared<sprite_entity>();
 				s->pos = position;
-				s->spr->size = size;
 				s->spr->layer = layer;
 				s->spr->colour = colour;
 				// s->sprite_path = path;
--- GameProject/src/scene.h
+++ GameProject/src/scene.h
@@ -10,30 +10,6 @@
 #include <unordered_map>
 
 //
-// PATH
-//
-
-typedef uint32_t PATH_NODE_FLAGS;
-enum PATH_NODE_FLAGS_ {
-	PATH_NODE_NONE		= 0,
-	PATH_NODE_SLOW		= 1 << 0,
-	PATH_NODE_FAST		= 1 << 1
-};
-static void PATH_NODE_FLAG_SET(PATH_NODE_FLAGS* x, PATH_NODE_FLAGS_ mask) { *x |= mask; }
-static void PATH_NODE_FLAG_CLEAR(PATH_NODE_FLAGS* x, PATH_NODE_FLAGS_ mask) { *x &= ~mask; }
-static void PATH_NODE_FLAG_TOGGLE(PATH_NODE_FLAGS* x, PATH_NODE_FLAGS_ mask) { *x ^= mask; }
-
-struct path_node {
-	glm::vec2 pos = { 0,0 };
-	PATH_NODE_FLAGS flags = 0;
-
-	std::string trigger_event_name = "";
-
-	std::vector<std::shared_ptr<enemy_entity>> enemies;
-	std::vector<std::shared_ptr<obstacle_entity>> obstacles;
-};
-
-//
 // EVENTS
 //
 
@@ -78,7 +54,8 @@ struct level {
 	event_manager game_event_manager;
 
 	std::vector<std::shared_ptr<cutscene>> cutscenes;
-	std::vector<std::shared_ptr<path_node>> path_nodes;
+	std::vector<std::shared_ptr<enemy_entity>> enemies;
+	std::vector<std::shared_ptr<obstacle_entity>> obstacles;
 
 	std::vector<std::shared_ptr<sprite_entity>> sprites;
 
