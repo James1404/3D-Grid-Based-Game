3cdce705b867d8ffcacd0085a6dceff1cf37ed93
James Barnfather <jimboblony@outlook.com>
2021-11-24 18:07:27 +0000

    added entity_manager. added common functions. added event and event_manager.

--- GameProject/src/entity.cpp
+++ GameProject/src/entity.cpp
@@ -1,41 +1,278 @@
 #include "entity.h"
 
-#include "scene.h"
 #include "player.h"
 
-obstacle_entity::obstacle_entity() {
-	spr = renderer::create_sprite();
-	spr->position = &pos;
-	spr->layer = -1;
-	spr->colour = colour::green;
+#include <fstream>
+#include <sstream>
+
+//
+// EVENTS
+//
+
+void event_manager::register_listener(std::string _event_name, listener* _listener) {
+	events.emplace(_event_name, _listener);
 }
 
-obstacle_entity::~obstacle_entity() {
-	renderer::delete_sprite(spr);
+void event_manager::remove_listener(std::string _event_name, listener* _listener) {
+	auto range = events.equal_range(_event_name);
+	for (auto i = range.first; i != range.second; ++i) {
+		if (i->second == _listener) {
+			events.erase(i);
+			break;
+		}
+	}
 }
 
-void obstacle_entity::update(double dt) {
+void event_manager::notify(std::string _event_name) {
+	auto range = events.equal_range(_event_name);
+	for (auto i = range.first; i != range.second; ++i) {
+		i->second->on_notify();
+	}
 }
 
-sprite_entity::sprite_entity() {
-	// sprite_path = "";
-
-	spr = renderer::create_sprite();
-	spr->position = &pos;
-	spr->layer = 0;
-	//spr->set_sprite_path(sprite_path.c_str());
-	spr->colour = { 0,0,0 };
+void event_manager::clear() {
+	events.clear();
 }
 
-sprite_entity::~sprite_entity() {
-	renderer::delete_sprite(spr);
+//
+// EVENT MANAGER
+//
+
+void entity_manager::init() {
+
 }
 
-void sprite_entity::update(double dt) {
-
+void entity_manager::update(double dt) {
+	for (auto& _entity : entities) {
+		_entity->update(dt);
+	}
 }
+
+void entity_manager::clean() {
+	game_event_manager.clear();
+
+	entities.clear();
+	entities_quick_tag_lookup.clear();
+
+	name.clear();
+
+	printf("CLEANED LEVEL DATA\n");
+}
+
+uint32_t fileVersion = 8;
+void entity_manager::save() {
+	std::string levelPath = "data/scenes/";
+	levelPath.append(name);
+	levelPath.append(".scene");
+
+	std::ofstream ofs(levelPath, std::ofstream::trunc);
+	if (ofs.is_open()) {
+		ofs << "FILE_VERSION " << fileVersion << std::endl << std::endl;
+
+		if (!entities.empty()) {
+			for (auto& _entity : entities) {
+				if (_entity->tag == "enemy") {
+					ofs << "ENEMY" << " " << (int)_entity->pos.x << " " << (int)_entity->pos.y << std::endl;
+				}
+			}
+
+			ofs << std::endl;
+		}
+	}
+
+	ofs.close();
+}
+
+void entity_manager::load(std::string level_name) {
+	printf("---------------------\n");
+	printf("RETRIEVING SCENE FILE\n");
+
+	clean();
+
+	std::string levelPath = "data/scenes/";
+	levelPath.append(level_name);
+	levelPath.append(".scene");
+
+	std::ifstream ifs(levelPath);
+	if (ifs.is_open()) {
+		printf("PARSING SCENE FILE\n");
+
+		std::string line;
+		while (std::getline(ifs, line)) {
+			std::stringstream ss(line);
+			std::string type;
+
+			std::getline(ss, type, ' ');
+
+			if (type == "FILE_VERSION") {
+				uint32_t file_version;
+				ss >> file_version;
+
+				if (file_version != fileVersion) {
+					printf("OUTDATED LEVEL FILE\n");
+					return;
+				}
+			}
+
+			if (type == "PLAYER") {
+				glm::ivec2 position;
+				ss >> position.x >> position.y;
+
+				auto p = std::make_shared<player_entity>();
+				p->manager = this;
+				p->pos = position;
+
+				entities.push_back(p);
+				entities_quick_tag_lookup.emplace(p->tag, p.get());
+			}
+			else if (type == "ENEMY") {
+				glm::ivec2 position;
+				ss >> position.x >> position.y;
+
+				auto e = std::make_shared<enemy_entity>();
+				e->manager = this;
+				e->pos = position;
+
+				entities.push_back(e);
+				entities_quick_tag_lookup.emplace(e->tag, e.get());
+			}
+		}
+
+		name = level_name;
+
+		printf("FINISHED LOADING SCENE DATA\n");
+		printf("---------------------------\n");
+	}
+	else {
+		printf("CANNOT FIND SCENE FILE\n");
+	}
+
+	ifs.close();
+}
+
+bool entity_manager::check_collisions(glm::vec2 _pos, std::string _tag) {
+	if (_tag.empty()) {
+		for (const auto& entity : entities) {
+			if (entity->flags & ENTITY_NO_COLLISION)
+				continue;
+
+			if (entity->pos == common::vec_to_ivec(_pos)) {
+				return true;
+			}
+		}
+	}
+	else {
+		auto range = entities_quick_tag_lookup.equal_range(_tag);
+		for (auto i = range.first; i != range.second; ++i) {
+			if (i->second->flags & ENTITY_NO_COLLISION)
+				continue;
+
+			if (i->second->pos == common::vec_to_ivec(_pos)) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+bool entity_manager::check_collisions(entity* _owner, glm::vec2 _pos, std::string _tag) {
+	if (_tag.empty()) {
+		for (const auto& entity : entities) {
+			if (entity.get() == _owner)
+				continue;
+
+			if (entity->flags & ENTITY_NO_COLLISION)
+				continue;
+
+			if (entity->pos == common::vec_to_ivec(_pos)) {
+				return true;
+			}
+		}
+	}
+	else {
+		auto range = entities_quick_tag_lookup.equal_range(_tag);
+		for (auto i = range.first; i != range.second; ++i) {
+			if (i->second == _owner)
+				continue;
+
+			if (i->second->flags & ENTITY_NO_COLLISION)
+				continue;
+
+			if (i->second->pos == common::vec_to_ivec(_pos)) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+entity* entity_manager::get_collisions(glm::vec2 _pos, std::string _tag) {
+	if (_tag.empty()) {
+		for (const auto& entity : entities) {
+			if (entity->flags & ENTITY_NO_COLLISION)
+				continue;
+
+			if (entity->pos == common::vec_to_ivec(_pos)) {
+				return entity.get();
+			}
+		}
+	}
+	else {
+		auto range = entities_quick_tag_lookup.equal_range(_tag);
+		for (auto i = range.first; i != range.second; ++i) {
+			if (i->second->flags & ENTITY_NO_COLLISION)
+				continue;
+
+			if (i->second->pos == common::vec_to_ivec(_pos)) {
+				return i->second;
+			}
+		}
+	}
+
+	return nullptr;
+}
+
+entity* entity_manager::get_collisions(entity* _owner, glm::vec2 _pos, std::string _tag) {
+	if (_tag.empty()) {
+		for (const auto& entity : entities) {
+			if (entity.get() == _owner)
+				continue;
+
+			if (entity->flags & ENTITY_NO_COLLISION)
+				continue;
+
+			if (entity->pos == common::vec_to_ivec(_pos)) {
+				return entity.get();
+			}
+		}
+	}
+	else {
+		auto range = entities_quick_tag_lookup.equal_range(_tag);
+		for (auto i = range.first; i != range.second; ++i) {
+			if (i->second == _owner)
+				continue;
+
+			if (i->second->flags & ENTITY_NO_COLLISION)
+				continue;
+
+			if (i->second->pos == common::vec_to_ivec(_pos)) {
+				return i->second;
+			}
+		}
+	}
+
+	return nullptr;
+}
+
+//
+// ENTITIES
+//
 
 enemy_entity::enemy_entity() {
+	tag = "enemy";
+
 	spr = renderer::create_sprite();
 	spr->position = &pos;
 	spr->layer = -1;
--- GameProject/src/entity.h
+++ GameProject/src/entity.h
@@ -1,27 +1,145 @@
 #pragma once
 #include <glm.hpp>
+#include <memory>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <map>
 
 #include "renderer.h"
 #include "collision.h"
+
+//
+// COMMON FUNCTIONS
+//
+
+namespace common {
+	inline float lerp(const float a, const float b, const float t) {
+		return (a * (1.0f - t) + (b * t));
+	}
+
+	inline glm::vec2 lerp(const glm::vec2 a, const glm::vec2 b, const float t) {
+		return glm::vec2(lerp(a.x, b.x, t), lerp(a.y, b.y, t));
+	}
+
+	inline glm::vec2 move_towards(const glm::vec2 pos, const glm::vec2 target, const float step) {
+		const glm::vec2 delta = target - pos;
+		const float len2 = glm::dot(delta, delta);
+
+		if (len2 < step * step)
+			return target;
+
+		const glm::vec2 direction = delta / glm::sqrt(len2);
+
+		return pos + step * direction;
+	}
+
+	inline glm::vec2 vec_floor(glm::vec2 vec) {
+		return glm::vec2(floorf(vec.x), floorf(vec.y));
+	}
+
+	inline glm::ivec2 vec_to_ivec(glm::vec2 vec) {
+		return (glm::ivec2)vec_floor(vec);
+	}
+
+	/*
+	inline bool check_collisions(entity_manager* _manager, glm::vec2 _pos, std::string _tag = "", entity* _hit_entity = nullptr) {
+		for (const auto& entity : _manager->entities) {
+			if (!_tag.empty() && entity->tag != _tag)
+				continue;
+
+			if (entity->flags & ENTITY_NO_COLLISION)
+				continue;
+
+			if (entity->pos == vec_to_ivec(_pos)) {
+				_hit_entity = entity.get();
+				return true;
+			}
+		}
+
+		return false;
+	}
+	*/
+}
+
+struct entity;
+
+//
+// EVENTS
+//
+
+struct listener {
+	virtual ~listener() {}
+	virtual void on_notify() = 0;
+};
+
+struct event_manager {
+	std::unordered_multimap<std::string, listener*> events;
+
+	void register_listener(std::string _event_name, listener* _listener);
+	void remove_listener(std::string _event_name, listener* _listener);
+	void notify(std::string _event_name);
+	void clear();
+};
+
+//
+// EVENT MANAGER
+//
+
+struct entity_manager {
+	std::string name;
+
+	event_manager game_event_manager;
+
+	std::vector<std::shared_ptr<entity>> entities;
+	std::multimap<std::string, entity*> entities_quick_tag_lookup;
+
+	void init();
+	void update(double dt);
+	void clean();
+
+	void save();
+	void load(std::string level_name);
+
+	// collisions
+	bool check_collisions(glm::vec2 _pos, std::string _tag = "");
+	bool check_collisions(entity* _owner, glm::vec2 _pos, std::string _tag = "");
+
+	entity* get_collisions(glm::vec2 _pos, std::string _tag = "");
+	entity* get_collisions(entity* _owner, glm::vec2 _pos, std::string _tag = "");
+};
+
+// 
+// Entities
+//
 
 typedef uint32_t ENTITY_FLAGS;
 enum ENTITY_FLAGS_ {
 	ENTITY_NONE = 0,
 	ENTITY_DISABLED = 1 << 0,
-	ENTTIY_INVISIBLE = 1 << 1,
+	ENTITY_NO_DAMAGE = 1 << 1,
+	ENTITY_NO_COLLISION = 1 << 2
 };
-static void ENTITY_FLAG_SET(ENTITY_FLAGS* x, ENTITY_FLAGS_ mask) { *x |= mask; }
-static void ENTITY_FLAG_CLEAR(ENTITY_FLAGS* x, ENTITY_FLAGS_ mask) { *x &= ~mask; }
-static void ENTITY_FLAG_TOGGLE(ENTITY_FLAGS* x, ENTITY_FLAGS_ mask) { *x ^= mask; }
+inline void ENTITY_FLAG_SET(ENTITY_FLAGS* x, ENTITY_FLAGS_ mask) { *x |= mask; }
+inline void ENTITY_FLAG_CLEAR(ENTITY_FLAGS* x, ENTITY_FLAGS_ mask) { *x &= ~mask; }
+inline void ENTITY_FLAG_TOGGLE(ENTITY_FLAGS* x, ENTITY_FLAGS_ mask) { *x ^= mask; }
 
 static uint32_t current_id = 0;
 struct entity {
 	uint32_t id;
-	ENTITY_FLAGS flags = 0;
+	ENTITY_FLAGS flags;
+	std::string tag;
+
+	entity_manager* manager;
 
 	glm::ivec2 pos;
 
-	entity() : id(current_id++), pos(0,0) {
+	int max_health_points = 3;
+	int current_health_points;
+
+	bool is_dead = false;
+
+	entity() : id(current_id++), flags(0), tag(""), pos(0, 0), current_health_points(3), is_dead(false) {
 		printf("INITIALIZED ENTITY %p\n", this);
 	}
 
@@ -30,41 +148,37 @@ struct entity {
 	}
 
 	virtual void update(double dt) {}
-};
 
-//
-// Commands
-//
+	void do_damage(int _damage) {
+		if (flags & ENTITY_NO_DAMAGE)
+			return;
 
-// 
-// Entities
-//
+		if (is_dead)
+			return;
 
-struct obstacle_entity : public entity {
-	renderer::sprite* spr;
+		current_health_points -= _damage;
 
-	obstacle_entity();
-	~obstacle_entity();
-	virtual void update(double dt);
-};
+		if (current_health_points <= 0)
+			is_dead = true;
+	}
 
-struct sprite_entity : public entity{
-	renderer::sprite* spr;
+	void knockback(glm::ivec2 _direction, int _range) {
+		if (flags & ENTITY_NO_DAMAGE)
+			return;
 
-	// std::string sprite_path;
+		for (int i = 0; i < _range + 1; i++) {
+			if (manager->check_collisions(pos + (_direction * i))) {
+				pos += _direction * i;
+				return;
+			}
+		}
 
-	sprite_entity();
-	~sprite_entity();
-	virtual void update(double dt);
+		pos += _direction * _range;
+	}
 };
 
 struct enemy_entity : public entity {
 	renderer::sprite* spr;
-
-	const int max_health_points = 3;
-	int current_health_points;
-
-	bool is_dead = false;
 
 	enemy_entity();
 	~enemy_entity();
