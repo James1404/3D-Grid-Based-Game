a3566d37dd3734aecb91bf08a28a0784b068da24
James Barnfather <jimboblony@outlook.com>
2021-12-10 18:18:46 +0000

    Moved player_states enum into class. Removed int steps from update_logic.

--- GameProject/src/player.h
+++ GameProject/src/player.h
@@ -4,16 +4,7 @@
 #include "entity.h"
 #include "renderer.h"
 #include "collision.h"
-
-enum class player_states {
-	IDLE = 0,
-	MOVE_UP,
-	MOVE_DOWN,
-	MOVE_LEFT,
-	MOVE_RIGHT,
-	ATTACK,
-	SHOOT
-};
+#include "common.h"
 
 struct player_entity : public entity {
 	renderer::sprite spr;
@@ -25,11 +16,21 @@ struct player_entity : public entity {
 	uint32_t shoot_end_time = 0;
 	const uint32_t shoot_cooldown_duration = 1000;
 
+	enum player_states {
+		IDLE = 0,
+		MOVE_UP,
+		MOVE_DOWN,
+		MOVE_LEFT,
+		MOVE_RIGHT,
+		ATTACK,
+		SHOOT
+	};
+
 	common::Limited_Queue<player_states, 2> state_queue;
 
 	player_entity();
 	~player_entity();
 	void update_input(double dt) override;
-	void update_logic(int steps) override;
+	void update_logic() override;
 	void update_visuals(double dt) override;
 };
