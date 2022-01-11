f52c3ccd84bb63d644de0b709c5e7495c77eef1a
James Barnfather <jimboblony@outlook.com>
2022-01-11 21:59:52 +0000

    Updated the editor.

--- GameProject/src/editor.cpp
+++ GameProject/src/editor.cpp
@@ -10,10 +10,13 @@
 #include "input.h"
 #include "renderer.h"
 #include "entity.h"
-#include "player.h"
 #include "camera.h"
+#include "log.h"
 
-editor::editor(entity_manager& _manager) : manager(&_manager) {
+#include "player.h"
+#include "world_entities.h"
+
+void editor_manager::init(entity_manager& _manager) {
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 
@@ -22,45 +25,169 @@ editor::editor(entity_manager& _manager) : manager(&_manager) {
 	ImGui_ImplSDL2_InitForOpenGL(renderer::window, renderer::context);
 	ImGui_ImplOpenGL3_Init("#version 330");
 
-	manager->init();
+	manager = &_manager;
 
-	//manager->cameras.add_camera("Editor");
+	logger::info("INITIALIZED EDITOR");
 }
 
-editor::~editor() {
-	manager->clean();
-
+void editor_manager::clean() {
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplSDL2_Shutdown();
 	ImGui::DestroyContext();
 
-	printf("-----------------\n");
-	printf("EDITOR CLEANED UP\n");
+	logger::info("-----------------\n");
+	logger::info("EDITOR CLEANED UP\n");
 }
 
-static void clear_selected(editor* _editor) {
-	_editor->current_entity = nullptr;
+void editor_manager::move_cursor(glm::vec3 _vel) {
+	if (_vel == glm::vec3(0))
+		return;
+
+	cursor_grid_pos += _vel;
+
+	if (is_grabbed) {
+		for (auto selected : selected_entities) {
+			if (auto tmp_entity = selected.lock()) {
+				tmp_entity->grid_pos += _vel;
+				tmp_entity->visual_pos = tmp_entity->grid_pos;
+			}
+		}
+	}
 }
 
-void editor::update(double dt) {
+void editor_manager::update(double dt) {
 	manager->cameras.set_camera("Editor");
-	/* TODO: reimplemented camera code
-	if (input::mouse_button_pressed(input::MOUSE_RIGHT)) {
-		//  TODO: convert mouse position to world space
-		//	which will fix panning speed.
-		glm::vec2 mouseDelta = { -input::get_mouse_delta().x, input::get_mouse_delta().y };
-		//view = glm::translate(view, { mouseDelta, 0.0f });
+
+	if (auto editor_cam = manager->cameras.get_camera("Editor").lock()) {
+		glm::vec3 offset = glm::vec3(0, 6, 5);
+		editor_cam->position = ((glm::vec3)cursor_grid_pos + offset + 0.5f);
+
+		editor_cam->rotation.x = 20;
 	}
 
-	// TODO: it zooms into the center of the game, not the center of the camera
-	// TODO: use mouse wheel not buttons
-	float zoom = 1;
-	if (input::key_pressed(SDL_SCANCODE_Q)) { zoom -= camera_zoom_speed * dt; }
-	if (input::key_pressed(SDL_SCANCODE_E)) { zoom += camera_zoom_speed * dt; }
-	*/
+	glm::ivec3 vel{ 0 };
+	if (!(input::button_down("MoveUp") && input::button_down("MoveDown"))) {
+		if (input::button_down("MoveUp")) {
+			vel = { 0,0, -1 };
+		}
+		else if (input::button_down("MoveDown")) {
+			vel = { 0,0, 1 };
+		}
+	}
+
+	if (!(input::button_down("MoveLeft") && input::button_down("MoveRight"))) {
+		if (input::button_down("MoveLeft")) {
+			vel = { -1,0,0 };
+		}
+		else if (input::button_down("MoveRight")) {
+			vel = { 1,0,0 };
+		}
+	}
+
+	if (input::button_down("IncreaseHeight")) {
+		vel = { 0,1,0 };
+	}
+	else if (input::button_down("DecreaseHeight")) {
+		vel = { 0,-1,0 };
+	}
+
+	move_cursor(vel);
+
+	renderer::debug::draw_box_wireframe(cursor_grid_pos, glm::vec3(1), colour::white);
+
+	for (auto selected : selected_entities) {
+		if (auto tmp_entity = selected.lock()) {
+			renderer::debug::draw_box_wireframe(tmp_entity->grid_pos, glm::vec3(1), colour::white);
+		}
+	}
+
+	if (input::key_down(SDL_SCANCODE_G)) {
+		if (!selected_entities.empty())
+			is_grabbed = !is_grabbed;
+		else
+			is_grabbed = false;
+	}
+
+	if (input::key_down(SDL_SCANCODE_F)) {
+		// TODO: skip collision testing instead for simple position testing
+		// because entites with NO_COLLISION flag arent selected.
+		auto entity = manager->get_collisions(cursor_grid_pos);
+		for (auto tmp_entity : selected_entities) {
+			if (entity.lock() == tmp_entity.lock()) {
+				logger::warning("Already selected entity");
+				return;
+			}
+		}
+
+		selected_entities.push_back(entity);
+	}
+
+	if (input::key_down(SDL_SCANCODE_R)) {
+		selected_entities.clear();
+		is_grabbed = false;
+	}
+
+	if (input::key_down(SDL_SCANCODE_X)) {
+		manager->remove_entity(manager->get_collisions(cursor_grid_pos));
+	}
+
+	if (input::key_down(SDL_SCANCODE_1)) {
+		if (!manager->check_collisions(cursor_grid_pos)) {
+			/*
+			auto new_entity = std::make_shared<block_entity>();
+			new_entity->grid_pos = cursor_grid_pos;
+			*/
+			manager->add_entity("block", uuid(), 0, cursor_grid_pos);
+		}
+		else {
+			logger::warning("Entity already in position");
+		}
+	}
+	else if (input::key_down(SDL_SCANCODE_2)) {
+		if (!manager->check_collisions(cursor_grid_pos)) {
+			/*
+			auto new_entity = std::make_shared<player_entity>();
+			new_entity->grid_pos = cursor_grid_pos;
+			*/
+
+			manager->add_entity("player", uuid(), 0, cursor_grid_pos);
+		}
+		else {
+			logger::warning("Entity already in position");
+		}
+	}
 }
 
-void editor::draw() {
+static void draw_entity_data(std::weak_ptr<entity> _entity) {
+	/* NOT SURE IF I WANT TO BE ABLE TO CHANGE ENTITY DATA? (MIGHT RE-ENABLE LATER)
+	*/
+	static const float button_width = 0.25f;
+	if (auto tmp_entity = _entity.lock()) {
+		ImGui::Text("uuid: %llu", tmp_entity->id);
+
+		if (ImGui::Button("DISABLED", { ImGui::GetWindowSize().x * button_width, 0.0f }))
+			ENTITY_FLAG_TOGGLE(tmp_entity->flags, ENTITY_DISABLED);
+
+		ImGui::SameLine();
+		ImGui::Text((tmp_entity->flags & ENTITY_DISABLED) ? "ON" : "OFF");
+
+		if (ImGui::Button("NO_COLLISION", { ImGui::GetWindowSize().x * button_width, 0.0f }))
+			ENTITY_FLAG_TOGGLE(tmp_entity->flags, ENTITY_NO_COLLISION);
+
+		ImGui::SameLine();
+		ImGui::Text((tmp_entity->flags & ENTITY_NO_COLLISION) ? "ON" : "OFF");
+
+		if (ImGui::Button("NO_CLIMB", { ImGui::GetWindowSize().x * button_width, 0.0f }))
+			ENTITY_FLAG_TOGGLE(tmp_entity->flags, ENTITY_NO_CLIMB);
+
+		ImGui::SameLine();
+		ImGui::Text((tmp_entity->flags & ENTITY_NO_CLIMB) ? "ON" : "OFF");
+
+		ImGui::Text("flags: %i", tmp_entity->flags);
+	}
+}
+
+void editor_manager::draw() {
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplSDL2_NewFrame(renderer::window);
 	ImGui::NewFrame();
@@ -79,14 +206,8 @@ void editor::draw() {
 		window_flags |= ImGuiWindowFlags_NoNav;
 
 		if (ImGui::Begin("OVERLAY", &p_open, window_flags)) {
-			ImGui::Text("Game Stats");
-
-			ImGui::Separator();
 			ImGui::Text("Resolution: %i / %i", renderer::screen_resolution_x, renderer::screen_resolution_y);
-			
-			ImGui::Separator();
-			ImGui::Text("Number of:");
-			ImGui::Text(" - Entities %i", manager->entities.size());
+			ImGui::Text("Number of entities %i", manager->entities.size());
 
 			ImGui::End();
 		}
@@ -100,7 +221,6 @@ void editor::draw() {
 		window_flags |= ImGuiWindowFlags_NoCollapse;
 		window_flags |= ImGuiWindowFlags_NoNav;
 		window_flags |= ImGuiWindowFlags_NoFocusOnAppearing;
-		window_flags |= ImGuiWindowFlags_MenuBar;
 
 		ImGui::SetNextWindowPos(ImVec2(work_size.x, 0), ImGuiCond_Always, ImVec2(1.0f, 0.0f));
 		ImGui::SetNextWindowSize(ImVec2(300.0f, work_size.y), ImGuiCond_Always);
@@ -113,7 +233,7 @@ void editor::draw() {
 					manager->save();
 				}
 				else {
-					printf("Name Empty\n");
+					logger::info("Name Empty\n");
 				}
 			}
 			
@@ -121,22 +241,24 @@ void editor::draw() {
 
 			if (ImGui::Button("Load")) {
 				if (!manager->name.empty()) {
-					clear_selected(this);
 					manager->load(manager->name);
 				}
 				else {
-					printf("Name Empty\n");
+					logger::info("Name Empty\n");
 				}
 			}
 
 			ImGui::SameLine();
 
 			if (ImGui::Button("Clear")) {
-				clear_selected(this);
 				manager->clean();
 			}
 
 			ImGui::Separator();
+
+			for (auto _entity : selected_entities) {
+				draw_entity_data(_entity);
+			}
 
 			ImGui::End();
 		}
--- GameProject/src/editor.h
+++ GameProject/src/editor.h
@@ -2,18 +2,19 @@
 #include "camera.h"
 #include "entity.h"
 
-struct editor {
+struct editor_manager {
 	entity_manager* manager;
 
-	float camera_zoom_speed = .001f;
-	glm::vec2 velocity;
-	float speed = 2;
+	glm::ivec3 cursor_grid_pos{ 0 };
 
 	// TODO: make this a weak_ptr
-	std::shared_ptr<entity> current_entity = nullptr;
+	std::vector<std::weak_ptr<entity>> selected_entities;
+	bool is_grabbed = false;
 
-	editor(entity_manager& _manager);
-	~editor();
+	void move_cursor(glm::vec3 _vel);
+	
+	void init(entity_manager& _manager);
+	void clean();
 
 	void update(double dt);
 	void draw();
