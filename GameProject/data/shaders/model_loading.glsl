7e6483a090b6e6e5d6efd00d35ac6fc0e15a006e
James Barnfather <jimboblony@outlook.com>
2022-01-25 11:40:53 +0000

    Switch from obj to gltf.

--- GameProject/data/shaders/model_loading.glsl
+++ GameProject/data/shaders/model_loading.glsl
@@ -23,9 +23,9 @@ out vec4 FragColor;
 
 in vec2 TexCoords;
 
-uniform sampler2D texture_diffuse1;
+uniform sampler2D Texture;
 
 void main()
 {
-	FragColor = texture(texture_diffuse1, TexCoords);
+	FragColor = texture(Texture, TexCoords);
 }
