e38192df163071f2befcf4a9812dd52287b923d2
James Barnfather <jimboblony@outlook.com>
2022-02-07 22:35:44 +0000

    Fixed bug where nothing would print without a string at the begining.

--- GameProject/src/log.h
+++ GameProject/src/log.h
@@ -9,6 +9,6 @@ inline void log(Args&& ..._args) {
 	std::cout << std::endl;
 }
 
-#define log_info(...) log(__VA_ARGS__);
+#define log_info(...) log("", __VA_ARGS__);
 #define log_warning(...) log("'", __FILE__, "' Warning at Line(", __LINE__, "): ", __VA_ARGS__);
 #define log_error(...) log("'", __FILE__, "' Error at Line(", __LINE__, "): ", __VA_ARGS__);
